71e1a602beffcc9c14fba6a2f2ffda4fab913d15
Amaurynyc <amaury@outlook.com>
2024-07-18 16:50:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ if user_input:
         completion = client.completions.create(
         model="palmyra-x-002-instruct",
         best_of=1,
-        prompt=f"{context}.{user_input}. The response to the question should be only be a couple of paragraphs",
+        prompt=f"{context}.{user_input}. The answer should only have a couple of paragraph. Pick the best answer",
         )
         response = completion.choices[0].text       
 
